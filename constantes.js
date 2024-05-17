3747e1b81d3a93500443ad914bac259ce7cc330a
solwinteer <156627225+solwinteer@users.noreply.github.com>
2024-05-17 16:47:33 -0300

    actualizado codigo

---
+++ constantes.js
@@ -0,0 +1,10 @@
+// constantes
+const TIPO_PRODUCTO = {
+    Comida: "COMIDA",
+    Juguete: "JUGUETE",
+    Higiene: "HIGIENE"
+}
+
+const NO_APLICA = "N/A";
+
+const CARRITO_COMPRAS_KEY = "carritoCompras";
