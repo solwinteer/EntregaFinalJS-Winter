ff4b686c87c080db9a639cd0fdf6c1ff518a5141
solwinteer <156627225+solwinteer@users.noreply.github.com>
2024-05-30 20:16:34 -0300

    actualizado proyecto

--- js/script.js
+++ js/script.js
@@ -4,24 +4,10 @@ const elementoMain = document.getElementsByTagName("main")[0];
 let spanCarritoCompras = document.querySelector(".carrito-compras-container span");
 const productosCarroCompras = document.getElementsByClassName("carro-compras-productos")[0]
 const carroComprasTotal = document.getElementsByClassName("carro-compras-total")[0]
-// declaro el array de productos
-const productos = [
-    new Producto(1, 'Satiety', 'Alimento balanceado para gato regulador de peso.', 'Royal Cannin', 30, TIPO_PRODUCTO.Comida, 'https://cdn.royalcanin-weshare-online.io/Ij93eoYBRYZmsWpcS_N5/v9/ar-l-satiety-feline-seco-vhn-01'),
-    new Producto(2, 'Light', 'Alimento balanceado para gato regulador de peso.', 'Royal Cannin', 25, TIPO_PRODUCTO.Comida, 'https://traviesospetshop.com.ar/wp-content/uploads/2023/01/light-gato-para-pagi-web.png'),
-    new Producto(3, 'Indoor', 'Alimento balanceado para gato de interior.', 'Royal Cannin', 20, TIPO_PRODUCTO.Comida, 'https://cdn.royalcanin-weshare-online.io/8j-CLIYBRYZmsWpcGfF-/v13/ar-l-indoor-seco-fhn-01'),
-    new Producto(4, 'Mini Adulto', 'Alimento balanceado para perro de tamaño chico.', 'Royal Cannin', 22, TIPO_PRODUCTO.Comida, 'https://catycanar.vtexassets.com/arquivos/ids/155397/47.jpg?v=637637870051770000'),
-    new Producto(5, 'Digestive', 'Alimento balanceado de facil digestión para perro de tamaño mediano.', 'Royal Cannin', 28, TIPO_PRODUCTO.Comida, 'https://faunatown.com.ar/wp-content/uploads/2021/07/FT-E-commerce.png'),
+const url = new URL('productos.json', window.location.href).href;
 
-    new Producto(6, 'Hueso', 'Hueso de cuero de vaca comestible.', NO_APLICA, 5, TIPO_PRODUCTO.Juguete, 'https://www.mivetshop.com.ar/media/catalog/product/cache/f45fac214c5ee5bb0b2a50e4e7188992/6/5/654561_mesa_de_trabajo_1_3.jpg'),
-    new Producto(7, 'Caña', 'Caña de juguete con forma de rata para gato.', NO_APLICA, 3, TIPO_PRODUCTO.Juguete, 'https://http2.mlstatic.com/D_NQ_NP_718219-MLU70707492873_072023-O.webp'),
-    new Producto(8, 'Pelota', 'Pelota de tenis.', NO_APLICA, 4, TIPO_PRODUCTO.Juguete, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYWSNj9Oi3SKc1XnyaTckTOjdc3ZowU5me4njH-JgnSg&s'),
-
-    new Producto(9, 'Piedritas sanitarias', 'Piedras sanitarias para gato de 1.5kg', 'Pichichos', 3.5, TIPO_PRODUCTO.Higiene, 'https://faunatown.com.ar/wp-content/uploads/2022/11/Productos-Web.png'),
-    new Producto(10, 'Piedritas sanitarias', 'Piedras aglomerantes sanitarias para gato de 4kg', 'Michis', 7, TIPO_PRODUCTO.Higiene, 'https://herspet.com/wp-content/uploads/2020/10/piedras-sanitarias-michi-feliz-10bolsas-de-18-kg-D_NQ_NP_764948-MLA41577055605_042020-F.jpg'),
-    new Producto(11, 'Shampoo para perro', 'Shampoo para perro de 200ml hipoalergénico', 'Mis perritos', 6, TIPO_PRODUCTO.Higiene, 'https://acdn.mitiendanube.com/stores/001/151/906/products/osspretpgp1-196570682675c509c716690723431774-640-0.jpeg'),
-]
-
-let productosFiltrados = Array.from(productos);
+let productos = [];
+let productosFiltrados = [];
 
 // agrega un producto al carrito de compras. si existe le sube la cantidad, si no lo agrega con cantidad 1 
 function agregarProductoAlCarrito(producto) {
@@ -39,7 +25,16 @@ function agregarProductoAlCarrito(producto) {
 
     updateCarrito();
     updateListaCarrito();
-    actualizarStorage();
+
+    Toastify({
+        text: producto.nombre + " agregado al carrito",
+        avatar: producto.urlImagen,
+        duration: 3000,
+        style: {
+            background: "#ffcc00",
+            color: "black"
+        }
+    }).showToast();
 }
 
 // resta un producto al carrito de compras
@@ -56,16 +51,11 @@ function restarProductoAlCarrito(producto) {
 
     updateCarrito();
     updateListaCarrito();
-    actualizarStorage();
 }
 
-function actualizarStorage() {
-    if (!carritoCompras || carritoCompras.length == 0) {
-        localStorage.setItem(CARRITO_COMPRAS_KEY, null);
-    }
-    else {
-        localStorage.setItem(CARRITO_COMPRAS_KEY, JSON.stringify(carritoCompras));
-    }
+function vaciarCarritoCompras() {
+    carritoCompras = [];
+    updateCarrito();
 }
 
 function updateCarrito() {
@@ -83,9 +73,10 @@ function updateCarrito() {
 }
 
 // muestra/oculta el carrito de compras, actualizando el texto correspondiente
-function mostrarCarritoCompras() {
+function mostrarCarritoCompras(visibilidad) {
     let divCarritoCompras = document.querySelector(".carro-compras");
-    let isVisible = divCarritoCompras.style.visibility === 'visible';
+
+    let isVisible =  visibilidad != undefined ? visibilidad : divCarritoCompras.style.visibility === 'visible';
 
     if (isVisible) {
         divCarritoCompras.style.visibility = "hidden";
@@ -104,6 +95,7 @@ function updateListaCarrito() {
         span.innerText = "El carrito de compras está vacio"
         productosCarroCompras.appendChild(span);
         carroComprasTotal.innerText = "";
+        document.getElementById("btnFinalizarCompra").style.display = "none";
         return;
     }
     let total = 0;
@@ -115,18 +107,20 @@ function updateListaCarrito() {
 
         let linkAgregar = document.createElement("a");
         linkAgregar.href = "#";
-        linkAgregar.onclick = function() { agregarAlCarrito(prod.Producto.id) };
+        linkAgregar.onclick = function () { agregarAlCarrito(prod.Producto.id) };
         linkAgregar.innerText = " + "
 
         let linkRestar = document.createElement("a");
         linkRestar.href = "#";
-        linkRestar.onclick = function() { restarAlCarrito(prod.Producto.id) };
+        linkRestar.onclick = function () { restarAlCarrito(prod.Producto.id) };
         linkRestar.innerText = " - "
-        
+
         div.appendChild(span);
         div.appendChild(linkAgregar);
         div.appendChild(linkRestar);
         productosCarroCompras.appendChild(div);
+
+        document.getElementById("btnFinalizarCompra").style.display = "inline-block";
         total += monto;
     });
 
@@ -160,6 +154,12 @@ function crearCardsProductos() {
 
     for (const propiedad in productosAgrupadosPorTipo) {
         const productosPorTipo = productosAgrupadosPorTipo[propiedad];
+
+        let tituloSeccion = document.createElement("h2");
+        tituloSeccion.innerText = propiedad;
+        tituloSeccion.className = "titulo-categoria"
+        elementoMain.appendChild(tituloSeccion);
+
         let elementoSection = elementoMain.appendChild(document.createElement("section"));
 
         productosPorTipo.forEach(producto => {
@@ -207,14 +207,58 @@ function restarAlCarrito(id) {
     restarProductoAlCarrito(producto);
 }
 
-// carga el carrito de compras del storage y lo actualiza
-function cargarCarritoCompras() {
-    var carritoStorage = localStorage.getItem(CARRITO_COMPRAS_KEY);
-    if (carritoStorage || carritoStorage?.length > 0) {
-        carritoCompras = JSON.parse(carritoStorage);
-        updateCarrito();
-    }
+// traer los productos desde la URL
+function cargarProductos() {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener el JSON: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(_productos => {
+            productos = _productos;
+            productosFiltrados = Array.from(productos);
+            crearCardsProductos();
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la operación fetch:', error);
+        });
 }
 
-crearCardsProductos();
-cargarCarritoCompras();
+document.addEventListener('DOMContentLoaded', function () {
+    var fechaVencimientoInput = document.getElementById('fechaVencimiento');
+    var inputmask = new Inputmask('99/99');
+    inputmask.mask(fechaVencimientoInput);
+});
+
+document.getElementById('btnComprar').addEventListener('click', function () {
+    document.getElementById("spinnerOverlay").style.display = "flex";
+
+    setTimeout(function () {
+        document.getElementById("spinnerOverlay").style.display = "none";
+
+        var modal = document.getElementById('tarjetaModal');
+        var bootstrapModal = bootstrap.Modal.getInstance(modal) || new bootstrap.Modal(modal);
+        bootstrapModal.hide();
+
+        Swal.fire({
+            title: "Compra realizada con éxito!",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(/images/trees.png)",
+            backdrop: `
+              rgba(0,0,123,0.4)
+              url("https://sweetalert2.github.io/images/nyan-cat.gif")
+              left top
+              no-repeat
+            `
+          });
+
+          mostrarCarritoCompras(true);
+          vaciarCarritoCompras();
+    }, 2000);
+});
+
+cargarProductos();
